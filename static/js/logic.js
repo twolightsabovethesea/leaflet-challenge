4c5f95a9c63cff3a92b32ffbaf867aaa35a6291a
Meredith Hopkins <twolightsabovethesea@gmail.com>
2021-09-30 12:16:12 -0400

    updated documentation

--- static/js/logic.js
+++ static/js/logic.js
@@ -38,7 +38,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
       attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     });
   
-    // Create a baseMaps object.
+    // creates a baseMaps object that includes the streetmap and topo layers
     var baseMaps = {
       "Street Map": streetmap,
       "Topographic Map": topo
